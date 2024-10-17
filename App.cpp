36cb60f02eabc5dc8255114485b928a7acbc1451
vieinfernale <weslleyskah@gmail.com>
2024-10-17 11:11:52 -0300

    Shader Program I

--- App.cpp
+++ App.cpp
@@ -77,8 +77,8 @@ struct Shader {
         // Read the shader source code
         const char* shaderSourceCodeCstr = shaderSourceCode.c_str();
 
-        /* This function assigns the shader source code to the shader object identified by shaderID. 
-        The first parameter is the shader object ID, the second is the number of strings being passed (1 in this case), 
+        // This function assigns the shader source code to the shader object identified by shaderID. 
+        /* The first parameter is the shader object ID, the second is the number of strings being passed (1 in this case), 
         and the third is the address of the source code string. 
         The nullptr for the final argument indicates that OpenGL can automatically determine the length of the source code string. */
 
@@ -135,12 +135,44 @@ struct Shader {
         glGenBuffers(1, &VBO);
 
         glBindVertexArray(VAO);
-
-        // Bind the VBO and upload the vertex data
         glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+        // Creates a vertex buffer, allocates memory for storing vertex data, uploads the vertex data to the GPU, and specifies that this data is for static use.
+        /*
+        GL_ARRAY_BUFFER: indicates that the buffer being created is for storing vertex attributes, such as vertex positions, colors, normals, etc.
+        vertices.size() * sizeof(vertex): calculates the total size of the data to be stored in the buffer.
+        vertices.data(): This provides a pointer to the actual vertex data that will be uploaded to the GPU. 
+        The data is stored in the CPU's memory, and this function transfers it to the GPU.
+        GL_STATIC_DRAW: This tells OpenGL the intended usage of the buffer. 
+        GL_STATIC_DRAW means that the data will be set once and used many times (static data, like vertex positions for a static mesh). 
+        */
         glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(vertex), vertices.data(), GL_STATIC_DRAW);
 
-        // Set up vertex attribute pointers
+        // Vertex attribute pointers
+        /*
+        This function specifies how OpenGL should interpret the vertex data within the buffer object, by linking it to a vertex attribute location in the shader.
+
+        0: This is the index of the vertex attribute in the shader. The number 0 refers to the first attribute location. 
+        This must correspond to the location used in the shader for the position attribute (layout(location = 0) in the vertex shader).
+
+        3: vertex position (x, y, z)
+        4: vertex color (r, g, b, a)
+        2: vertex texture (u, v)
+
+        GL_FLOAT: This specifies the data type of each component in the vertex attribute. GL_FLOAT means each of the 3 components is a floating-point value.
+
+        GL_FALSE: This specifies whether the data should be normalized. GL_FALSE means that the data will not be normalized and will be used as is.
+
+        sizeof(vertex): byte size between consecutive vertices. 1 vertice = {{x,y,z},{r,g,b,a},{u,v}}
+        The stride tells how far apart the data for each vertex is in the buffer.
+
+        (void*)offsetof(vertex, vp): This is the pointer to the start of the vertex position data within the vertex structure. 
+        offsetof(vertex, vp) calculates the byte offset of the vp (vertex position) field within the vertex structure. 
+        Casting it to (void*) tells OpenGL where the first component of the vertex position is located in memory.
+        */
+        /*
+        Enables the vertex attribute at index 0, 1, and 2 (the layout locations), so it can be used during rendering.
+        */
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(vertex), (void*)offsetof(vertex, vp));
         glEnableVertexAttribArray(0);
 
@@ -150,6 +182,12 @@ struct Shader {
         glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(vertex), (void*)offsetof(vertex, vt));
         glEnableVertexAttribArray(2);
 
+        // Ubind the VAO
+        /*
+        You generally unbind the VAO after configuring it to prevent accidental changes to it later. If no VAO is bound, any subsequent OpenGL calls 
+        that modify vertex attributes will not affect this VAO.
+        When it's time to render, you will bind this VAO again to use the configurations you've set up earlier.
+        */
         glBindVertexArray(0);
     }
 
@@ -158,8 +196,16 @@ struct Shader {
         // Use the shader program
         glUseProgram(shaderProgramID);
 
-        // Bind the VAO and draw the triangle
+        // Bind the VAO
+        /*
+        Once the VAO is bound, all subsequent vertex attribute and buffer state changes affect this VAO until another VAO is bound or unbound. 
+        Therefore, binding the VAO ensures that OpenGL has the correct vertex configuration before issuing a draw call.
+        OpenGL is stateful: OpenGL is a state machine, which means it doesn't "remember" the VAO you used in the last draw call once it is unbound. 
+        If another part of your program (or OpenGL itself) changes the VAO or other states, OpenGL won't automatically know which VAO you intend to use for a draw call. 
+        Binding the VAO before drawing ensures that the correct vertex attributes and buffer configurations are used.
+        */
         glBindVertexArray(VAO);
+
         glDrawArrays(GL_TRIANGLES, 0, vertices.size());
     }
 
